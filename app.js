ce8909e997ea61b7a7a8752af59cdc4c72efce6b
s0ulcat <babich.danya30@gmail.com>
2024-04-25 09:41:21 +0300

    super mario

---
+++ app.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { error } = require('console');
+const PORT = 1488;
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.waw': 'audio/waw',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+};
+
+function staticFile (res, filePath, ext) {
+    res.setHeader("Content-type", mimeTypes[ext]);
+    fs.readFile('./public' + filePath, {}, (error, data) => {
+        if (error){
+            res.statusCode = 404;
+            res.end();
+        }
+        res.end(data);
+    });
+}
+
+http.createServer(function(req,res){
+    const url = req.url;
+    console.log(url);
+
+    switch (url) {
+        case '/':
+            console.log('home page');
+            res.write('<h1>Home</h1>');
+            res.end();
+            break;
+        case '/contact':
+            console.log('contact page');
+            staticFile(res, '/contact.html', '.html');
+            break;
+        default:
+            const extname = String(path.extname(url)).toLocaleLowerCase();
+            if (extname in mimeTypes) {
+                staticFile(res, url, extname);
+            }
+            else{
+                res.statusCode = 404;
+                res.end();
+            }
+    }
+}).listen(PORT);
